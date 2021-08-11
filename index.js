482b1563e83a39aaf0895bfda50ef4fb4d05e509
Jennifer Haggerty <thejenniferhaggerty@gmail.com>
2021-08-10 22:36:55 -0400

    Working. The delay will not work if a frontend is added later, at least not without a loading spinner, at that time there may be a new option or learned method to take care of it.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const axios           = require('axios');
-const bluebird 				= require('bluebird');
-const { response } = require('express');
-const createTransport = require('nodemailer');
+const nodemailer		 	= require('nodemailer');
 const config 					= require('./config');
-var boards 					= config.eventBoards;
-var whiteList				= config.whiteList;
+var boards 						= config.eventBoards;
+var whiteList					= config.whiteList;
+var email 						= config.email;
+var password 					= config.password;
 
 if(!config) {
   console.log("You need a config/ file!");
@@ -14,12 +14,10 @@ main().catch(console.error);
 
 async function main() {
 	var endpoints = makeEndpoints(boards, whiteList);
-	var events = await getData(endpoints);
-	return;
-	cleanData(events.length); return;
+	var data = await getData(endpoints);
+	var events = cleanData(data);
 	if (events.length < 1) { return; }
-	var eventDetails = cleanData(events);
-	var message = prepareMessage(eventDetails);
+	var message = prepareMessage(events);
 	sendEmail(message);
 }
 function makeEndpoints(boards, whiteList) {
@@ -37,30 +35,26 @@ async function getData(endpoints) {
 	for (i = 0; i < endpoints.length; i ++) {
 		let url = endpoints[i];
 		axios.get(url).then(res => {
-			console.log(res.status);
+			if (res.status == 200 && res.data._embedded && res.data._embedded.events) {
+				let json = res.data._embedded.events;
+				data.push(json);
+			}
 		})
-		.catch(err => {
-			console.log(err.toString());
-		})
+		.catch(err => { console.log(err.toString()); })
 		await sleep(1000);
 	}
+	return data;
 }
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
 function cleanData(jsonArr) {
 	let newJsonArr = [];
 	jsonArr.forEach(json => {
-		let venue 				= json._embedded.venues[0];
+		let venue 				= json[0]._embedded.venues[0];
 		let newJson 			= {};
-		newJson.name			= json.name;
+		newJson.name			= json[0].name;
 		newJson.location 	= venue.city['name'] + "," + venue.state["stateCode"];
-		newJson.url 			= json.url;
-		newJson.startTime = json.dates.start.localTime;
-		newJson.startDate = json.dates.start.localDate;
-		
+		newJson.url 			= json[0].url;
+		newJson.startTime = json[0].dates.start.localTime;
+		newJson.startDate = json[0].dates.start.localDate;
 		newJsonArr.push(newJson);
 	});
 	return newJsonArr;
@@ -69,36 +63,30 @@ function prepareMessage(events) {
 	var message = '';
 	events.forEach(event => {
 		var date = new Date(event.startDate);
-		message =  message + "\n" +s 
+		message =  message + "\n" +
 			event.name + "\n" +
 			date + "\n" + 
-			event.location + "\n" + 
-			event.url + "\n\n"
-		;
+			event.location + "\n" +
+			event.url + "\n\n";
 	});
 	return message;
 }
 function sendEmail(textMessage){
-	var transporter = createTransport({
+	var transporter = nodemailer.createTransport({
 		service: 'gmail',
-		auth: {
-		  user: email,
-		  pass: password
-		}
-	  });
-	  
-	  var mailOptions = {
+		auth: { user: email, pass: password }
+	});
+	var mailOptions = {
 		from: "Moshbot 🤘 " + email,
 		to: email,
 		subject: "MoshBot found some concerts",
 		text: textMessage
-	  };
-	  
-	  transporter.sendMail(mailOptions, function(error, info){
-		if (error) {
-		  console.log(error);
-		} else {
-		  console.log('Email sent: ' + info.response);
-		}
-	  });
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) { console.log(error); } 
+		else { console.log('Email sent: ' + info.response); }
+	});
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
