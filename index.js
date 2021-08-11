17f29226b77bd19968035111b9264d62aa68ad77
Jennifer Haggerty <thejenniferhaggerty@gmail.com>
2021-08-10 21:19:34 -0400

    After MANY attempts to run proimises against Ticketmaster to get all event details as speedy as possible it was decided that it is not possible as ticketmaster limits x5 requests per second and there isn't a way (that I know of) to halt the async requests. To allow all 72 (present endpoint count) to return status 200, each requests has a 1 second delay after it has been sent so as not to flood Ticketmaster and be flagged as a DDoS. Recording the chunking code of the endpoints array into 5 chunks in case something gets discovered/learned later

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var axios 		= require('axios');
-var nodemailer	= require('nodemailer');
-var config 		= require('./config');
-var boards 		= config.eventBoards;
-var whiteList	= config.whiteList;
+const axios           = require('axios');
+const bluebird 				= require('bluebird');
+const { response } = require('express');
+const createTransport = require('nodemailer');
+const config 					= require('./config');
+var boards 					= config.eventBoards;
+var whiteList				= config.whiteList;
 
 if(!config) {
   console.log("You need a config/ file!");
@@ -11,40 +13,87 @@ if(!config) {
 main().catch(console.error);
 
 async function main() {
-	var urls = makeUrls(boards, whiteList);
-	var events = await getData(urls);
+	var endpoints = makeEndpoints(boards, whiteList);
+	var events = await getData(endpoints);
+	return;
+	cleanData(events.length); return;
 	if (events.length < 1) { return; }
 	var eventDetails = cleanData(events);
 	var message = prepareMessage(eventDetails);
 	sendEmail(message);
 }
+function makeEndpoints(boards, whiteList) {
+	let endpoints = [];
+	boards.forEach(board => {
+		whiteList.forEach(keyword => {
+			keyword = keyword.replace(/\s+/g, '%').toLowerCase();
+			endpoints.push(board + "&keyword=" + keyword);
+		});
+	});
+	return endpoints;
+}
+async function useAxios(url) {
+	try {
+		const {data:response} = await axios.get(url);
+		console.log(response.status)
+    return response
+	} catch (error) {
+		console.log(error.toString());
+	}
+}
+async function getData(endpoints) {
+	let data = [];
+	let k = 0;
+	for (i = 0; i < endpoints.length; i ++) {
+		let url = endpoints[i];
+		axios.get(url).then(res => {
+			console.log(res.status);
+		})
+		.catch(err => {
+			console.log(err.toString());
+		})
+		await sleep(1000);
+	}
+	/*
+	for (i = 0; i < endpoints.length; i += 5) {
+		let arrChunk = endpoints.slice(i, i + 5);
+		for (j = 0; j < arrChunk.length; j++) {
+			let url = arrChunk[j];
+			axios.get(url).then(res => {
+				console.log(res.status);
+			})
+			.catch(err => {
+				console.log(err.toString());
+			})
+			await sleep(1000);
+		}
+		k++;
+		console.log(i);
+	}
+	*/
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function sendEmail(textMessage){
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-		  user: config.email,
-		  pass: config.password
-		}
-	  });
-	  
-	  var mailOptions = {
-		from: "Moshbot 👻 " + config.email,
-		to: config.email,
-		subject: "MoshBot found some concerts!",
-		text: textMessage
-	  };
-	  
-	  transporter.sendMail(mailOptions, function(error, info){
-		if (error) {
-		  console.log(error);
-		} else {
-		  console.log('Email sent: ' + info.response);
-		}
-	  });
+
+function cleanData(jsonArr) {
+	let newJsonArr = [];
+	jsonArr.forEach(json => {
+		let venue 				= json._embedded.venues[0];
+		let newJson 			= {};
+		newJson.name			= json.name;
+		newJson.location 	= venue.city['name'] + "," + venue.state["stateCode"];
+		newJson.url 			= json.url;
+		newJson.startTime = json.dates.start.localTime;
+		newJson.startDate = json.dates.start.localDate;
+		
+		newJsonArr.push(newJson);
+	});
+	return newJsonArr;
 }
 function prepareMessage(events) {
-	var message = `Ready the battle dress: `;
+	var message = '';
 	events.forEach(event => {
 		var date = new Date(event.startDate);
 		message =  message + "\n" +s 
@@ -56,28 +105,43 @@ function prepareMessage(events) {
 	});
 	return message;
 }
-function cleanData(jsonArr) {
-	var eventDetails = [];
-	jsonArr.forEach(json => {
-		var venue 			= json._embedded.venues[0];
-		var newJson 		= {};
-		newJson.name		= json.name;
-		newJson.location 	= venue.city['name'] + "," + venue.state["stateCode"];
-		newJson.url 		= json.url;
-		newJson.startTime 	= json.dates.start.localTime;
-		newJson.startDate 	= json.dates.start.localDate;
-		
-		eventDetails.push(newJson);
-	});
-	return eventDetails;
+function sendEmail(textMessage){
+	var transporter = createTransport({
+		service: 'gmail',
+		auth: {
+		  user: email,
+		  pass: password
+		}
+	  });
+	  
+	  var mailOptions = {
+		from: "Moshbot 🤘 " + email,
+		to: email,
+		subject: "MoshBot found some concerts",
+		text: textMessage
+	  };
+	  
+	  transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+		  console.log(error);
+		} else {
+		  console.log('Email sent: ' + info.response);
+		}
+	  });
 }
-async function getData(urls) {
-	var events = [];
-	for (x = 0; x < urls.length; x++) {
+
+
+/*async function getData(endpoints) {
+	let events = [];
+	for (x = 0; x < endpoints.length; x++) {
 		try {
-			const response = await axios.get(urls[x]);
-			if (response.status == 200 &&  response.data._embedded.events != undefined) {
-				var eventsArr = response.data._embedded.events;
+			let response = await axios.get(endpoints[x], {timeout: 200});
+			//console.log(response);
+			if (	response.status == 200 && 
+						response.data.hasOwnProperty('_embedded') && 
+						response.data._embedded.hasOwnProperty('events')
+					) {
+				let eventsArr = response.data._embedded.events;
 				eventsArr.forEach(event => {
 					events.push(event)
 				});
@@ -86,14 +150,7 @@ async function getData(urls) {
 			console.log(error);
 		}
 	}
+	console.log(events);
 	return events;
 }
-function makeUrls(boards, whiteList) {
-	var urls = [];
-	boards.forEach(board => {
-		whiteList.forEach(name => {
-			urls.push(board + "&keyword=" + name);
-		});
-	});
-	return urls;
-}
+*/
